bf17817fb16095ce9a2937e99a9fbdf2d2a9a225
Vitor Sales <vitorsalesdev@gmail.com>
2020-09-21 14:54:05 -0300

    Sql exemplo 1

--- aula21-09/aulaCreateSql.sql
+++ aula21-09/aulaCreateSql.sql
@@ -1,6 +1,12 @@
-#cria o banco
-create database if not exists google;
-#ativa o banco
-use google;
-#mostra os databases
-show databases;
+create database if not exists db_loja1;
+use db_loja1;
+CREATE TABLE if not exists tb_clientes
+(
+codigo INT,
+nome VARCHAR(60),
+dataNascimento DATE,
+telefone CHAR (8)
+);
+
+select * from tb_clientes;
+describe tb_clientes;
